c0a5758eaf45a5abe37aa210117f32f466a8d035
kunalgoenka19 <kunalgoenka915@gmail.com>
2026-09-06 22:03:45 +0530

    task based

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/student.java
@@ -0,0 +1,8 @@
+public class student {
+    public static void main(String[]args){
+    // Display the students's learning track.
+    System.out.println("Track: Java Developement");
+    // Display the student's daily commitment.
+    System.out.println("Daily Practice: 8 Hours");
+    }
+}
