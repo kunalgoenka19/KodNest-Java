490ee775384f5aed08598a32e51794dc819ab101
kunalgoenka19 <kunalgoenka915@gmail.com>
2026-09-06 22:11:12 +0530

    my placement goal

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/placement.java
@@ -0,0 +1,8 @@
+public class placement {
+    public static void main(String[]args){
+    System.out.println("--- My Placement Goal---");
+    System.out.println("Role: Software Developer");
+    System.out.println("Target: Get Placed");
+    System.out.println("Daily Practice: 8 hours");
+    }
+}
